72a346db3cb4c1e4bbe47ea8bd10368725eb49d7
GuyTito <35842294+GuyTito@users.noreply.github.com>
2022-03-31 13:21:56 +0000

    implement style guide

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,26 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
   content: [
     "./index.html",
     "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "Moderate-blue": "hsl(238, 40%, 52%)",
+        "Soft-Red": "hsl(358, 79%, 66%)",
+        "Light-grayish-blue": "hsl(239, 57%, 85%)",
+        "Pale-red": "hsl(357, 100%, 86%)",
+        "Dark-blue": "hsl(212, 24%, 26%)",
+        "Grayish-Blue": "hsl(211, 10%, 45%)",
+        "Light-gray": "hsl(223, 19%, 93%)",
+        "Very-light-gray": "hsl(228, 33%, 97)",
+      },
+      fontFamily: {
+        sans : ["'Rubik'", ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
   plugins: [],
 }
