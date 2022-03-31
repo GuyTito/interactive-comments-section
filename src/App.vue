f7d904925602cb06398e844661db8db604edbe82
GuyTito <35842294+GuyTito@users.noreply.github.com>
2022-03-31 13:02:40 +0000

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,9 @@
 <template>
   <div>
     App.vue
+    <!-- Pull dynamic content from the data.json file -->
   </div>
+  
   <footer class="mt-10 mb-2 text-center text-xs text-Dark-gray">
     Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank" class="underline hover:font-bold">Frontend Mentor</a>. 
       Coded by <a href="https://github.com/GuyTito/crowdfunding-product-page"  target="_blank" class="underline hover:font-bold">GuyTito</a>.
