7060b3d42c306f2ea63ea901f40324ff07a95ade
GuyTito <35842294+GuyTito@users.noreply.github.com>
2022-03-31 12:38:00 +0000

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -7,6 +7,10 @@
   <div>
     App.vue
   </div>
+  <footer class="mt-10 mb-2 text-center text-xs text-Dark-gray">
+    Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank" class="underline hover:font-bold">Frontend Mentor</a>. 
+      Coded by <a href="https://github.com/GuyTito/crowdfunding-product-page"  target="_blank" class="underline hover:font-bold">GuyTito</a>.
+  </footer>
 </template>
 
 
