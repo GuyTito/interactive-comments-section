3c50f5b8db8e5e3002d681826ae9ef7458c0b514
GuyTito <35842294+GuyTito@users.noreply.github.com>
2022-04-01 11:14:30 +0000

    display comments

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,8 @@ import { ref } from 'vue';
 import comments from '@/composables/data.js'
 
 const data = ref(null)
-// check if we have comments in localstorage
+const currentComment = ref('')
+// check for comments in localstorage and retrieve
 if(!localStorage.getItem('comments')) {
   localStorage.setItem('comments', JSON.stringify(comments))
 } else {
@@ -14,15 +15,27 @@ if(!localStorage.getItem('comments')) {
 
 
 <template>
-  <div class="text-Moderate-blue ">
     <!-- Pull dynamic content from the data.json file -->
-    {{data}}
+  <div >
+    <div v-for="comment in comments" :key="comment.id">
+      <p class="mb-3">{{ comment.content }}</p>
+      <template v-if="comment.replies">
+        <div v-for="reply in comment.replies" :key="reply.id" class="ml-4 mb-3">
+          {{ reply.content }}
+        </div>
+      </template>
+    </div>
   </div>
 
-  <footer class="mt-10 mb-2 text-center text-xs text-Dark-gray">
+  <div>
+    <textarea v-model="currentComment" placeholder="Add a comment..." class="border-2"></textarea>
+    <button @click="send">Send</button>
+  </div>
+
+  <!-- <footer class="mt-10 mb-2 text-center text-xs text-Dark-gray">
     Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank" class="underline hover:font-bold">Frontend Mentor</a>. 
-      Coded by <a href="https://github.com/GuyTito/crowdfunding-product-page"  target="_blank" class="underline hover:font-bold">GuyTito</a>.
-  </footer>
+      Coded by <a href="https://github.com/GuyTito/interactive-comments-section"  target="_blank" class="underline hover:font-bold">GuyTito</a>.
+  </footer> -->
 </template>
 
 
