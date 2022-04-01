c238ef19b810ce1bb8432996f12a1aafb44041ef
GuyTito <35842294+GuyTito@users.noreply.github.com>
2022-04-01 10:06:57 +0000

    testing localstorage

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,22 @@
-<script setup>
+<script setup>import { ref } from 'vue';
+
+const data = ref(null)
+// check if we have comments in localstorage
+if(!localStorage.getItem('comments')) {
+  const comments = {boy: 'kofi', girl: 'ama'} 
+  localStorage.setItem('comments', JSON.stringify(comments))
+} else {
+  data.value = JSON.parse(localStorage.getItem('comments'))
+
+}
 
 </script>
 
 
 <template>
   <div class="text-Moderate-blue ">
-    App.vue
     <!-- Pull dynamic content from the data.json file -->
+    {{data.girl}}
   </div>
 
   <footer class="mt-10 mb-2 text-center text-xs text-Dark-gray">
