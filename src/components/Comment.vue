41a589ee66502a28a8809a3b7eb283d99564e09d
GuyTito <35842294+GuyTito@users.noreply.github.com>
2022-04-01 15:32:41 +0000

    Create Comment.vue

---
+++ src/components/Comment.vue
@@ -0,0 +1,38 @@
+<script setup>
+import ReplyIcon from "./icons/ReplyIcon.vue";
+const props = defineProps(['comment', ])
+
+</script>
+
+
+<template>
+  <div class="bg-white rounded-lg mx-4 p-4 mt-4 space-y-4 text-Grayish-Blue">
+    <div class="space-x-3">
+      <img :src="`/src/${comment.user.image.png}`" alt="avatar" class="h-8 w-8 inline">
+      <span class="font-bold text-Dark-blue">{{comment.user.username}}</span>
+      <span> {{comment.createdAt}} </span>
+    </div>
+
+    <p class="text-[16px] ">
+      {{ comment.content }}
+    </p>
+
+    <div class="text-Moderate-blue flex justify-between">
+      <div class="space-x-3 bg-Very-light-gray py-2 px-3 rounded-lg">
+        <button class="text-Light-grayish-blue ">+</button>
+        <span class="font-medium">{{comment.score}}</span>
+        <button class="text-Light-grayish-blue">-</button>
+      </div>
+
+      <button class="font-bold flex items-center space-x-2">
+        <i> <ReplyIcon /> </i>
+        <span>Reply</span>
+      </button>
+    </div>
+  </div>
+</template>
+
+
+<style lang="scss" scoped>
+
+</style>
