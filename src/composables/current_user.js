3f7e66215ae9cca70679934b9ee3c63b17f0016e
GuyTito <35842294+GuyTito@users.noreply.github.com>
2022-04-01 11:54:52 +0000

    Create current_user.js

---
+++ src/composables/current_user.js
@@ -0,0 +1,10 @@
+const current_user = 
+{
+  "image": { 
+    "png": "./images/avatars/image-juliusomo.png",
+    "webp": "./images/avatars/image-juliusomo.webp"
+  },
+  "username": "juliusomo"
+}
+
+export default current_user
