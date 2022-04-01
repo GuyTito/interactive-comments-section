98ef47a5dda7a6137149548bc551ff3793dd4132
GuyTito <35842294+GuyTito@users.noreply.github.com>
2022-04-01 16:23:27 +0000

    Create RDEButton.vue

---
+++ src/components/RDEButton.vue
@@ -0,0 +1,13 @@
+<script setup>
+
+</script>
+
+
+<template>
+  <button class="font-bold flex items-center space-x-2">
+    <i> 
+      <slot name="icon"></slot> 
+    </i>
+    <span><slot></slot></span>
+  </button>
+</template>
