6910f52611405b9db5c5eacb5ca941a2a2611e48
GuyTito <35842294+GuyTito@users.noreply.github.com>
2022-04-02 08:19:16 +0000

    Create Avatar.vue

---
+++ src/components/Avatar.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { computed } from '@vue/reactivity';
+
+const props = defineProps(['avatar_path', ])
+
+const img_src = computed(()=>{
+  return `/src/${props.avatar_path}`
+})
+
+</script>
+
+
+<template>
+  <img :src="img_src" alt="avatar" class="h-8 w-8 inline">
+</template>
