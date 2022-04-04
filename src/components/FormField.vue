e1fc13e7b7d43af39c74d5a264360b9d2b32b4dd
GuyTito <35842294+GuyTito@users.noreply.github.com>
2022-04-04 18:25:04 +0000

    Update FormField.vue

--- src/components/FormField.vue
+++ src/components/FormField.vue
@@ -5,22 +5,22 @@ const emit = defineEmits()
 const props = defineProps(['place_holder', 'purpose', 'content', ])
 
 const new_content = ref('')
-const updated_content = computed({
-  get: () => {
-    if (props.content) {
-      return new_content.value + props.content
-    }else{
-      return new_content.value
-    }
-  },
-  set: (update) => {
-    if (props.content) {
-      return props.content = props.content + update
-    }
-  }
-})
+// const updated_content = computed({
+//   get: () => {
+//     if (props.content) {
+//       return new_content.value + props.content
+//     }else{
+//       return new_content.value
+//     }
+//   },
+//   set: (update) => {
+//     if (props.content) {
+//       return props.content = props.content + update
+//     }
+//   }
+// })
 const submit = ()=> {
-  emit('action', updated_content.value)
+  emit('action', new_content.value)
   new_content.value = ''
 }
 </script>
@@ -28,7 +28,7 @@ const submit = ()=> {
 <template>
   <div class="bg-white rounded-lg mx-4 p-4 mt-8 space-y-4 text-Grayish-Blue">
     <span v-if="purpose">{{purpose}}</span>
-    <textarea v-model="updated_content" :placeholder="place_holder" class="border-2 rounded-lg h-28 w-full p-4"></textarea>
+    <textarea v-model="new_content" :placeholder="place_holder" class="border-2 rounded-lg h-28 w-full p-4"></textarea>
     
     <div class="flex justify-between items-center">
       <slot name="avatar"></slot>
